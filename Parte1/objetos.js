5fd441dd0e6ce7fdbf453b08692304a22f07597f
judy <judysantos169@gmail.com>
2024-08-31 23:35:06 -0300

    alteração

--- Parte1/objetos.js
+++ Parte1/objetos.js
@@ -1,16 +1,17 @@
-var rotina ={
-    'segunda' :'Matemtica',
-    'terça' :'Portugues',
-    'quarta': 'hacking'
+
+// objeto
+let rotina ={
+    'segunda' :'Fazer compras',
+    'terça' :'Ir ao mercado',
+    'quarta': ' Estudar hacking'
 }
 console.log(rotina)
-//Objeto retornará uma matriz de strings
-console.log(Object.keys({x :0, y :1, k :2}))
+
+console.log(Object.keys({x :0, y :1, k :2})) // retorna um array, 
 
 
-//copia todas as propriedades de um objeto para outro
 let objetoX = {a:2, b:9}
-Object.assign(objetoX, {b:8, c: 4})
+Object.assign(objetoX, {b:8, c: 4}) //copia as propriedades do objeto
 console.log(objetoX)
 
 
@@ -18,13 +19,14 @@ let object1 = {value: 10};
 let object2 = object1;
 let object3 = {value: 10};
 
-console.log(object1 == object2);
-// → true
-console.log(object1 == object3);
-// → false  pois são objetos que ocupam lugares diferentes na memória, objetos em JavaScript verifica se eles são a mesma instância, ou seja, se ocupam o mesmo espaço na memória, e não apenas se seus conteúdos são iguais.
+console.log(object1 == object2); //true
 
+console.log(object1 == object3); // false . pois é uma instância separada, oculpa outra parte da mémoria.
 
+// função que calcula o coeficiente de correlação do pi
 function phi(table) {
+
+  //A função recebe como argumento um array table com 4 valores
     return (table[3] * table[0] - table[2] * table[1]) /
       Math.sqrt((table[2] + table[3]) *
                 (table[0] + table[1]) *
@@ -32,16 +34,16 @@ function phi(table) {
                 (table[0] + table[2]));
   }
 
+
   function city(object) {
     return object.address?.city;
   }
   console.log(city({address: {city: "Toronto"}}));
-  // → Toronto
+  
   console.log(city({name: "Vera"}));
-  // → undefined
-
+  
   console.log("string".notAMethod?.());
-  // → undefined
+  
   console.log({}.arrayProp?.[0])
  
 
